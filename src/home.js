25ff65938c034161053e7eae6c64b4d88e6086a5
Samuel Alvarenga <69541465+samueledu10@users.noreply.github.com>
2023-11-20 17:33:02 -0500

    Add home module to page

--- src/home.js
+++ src/home.js
@@ -1,4 +1,17 @@
 
 export function home() {
-    
+    const content = document.querySelector("#content");
+    const contentBody = document.createElement("div");
+    contentBody.classList.add("content-body");
+
+    const homeContainer = document.createElement("div");
+    homeContainer.classList.add("home-container");
+
+    const headline = document.createElement("h2");
+    headline.classList.add("headline");
+    headline.textContent = "Best Pizza Experience!";
+
+    homeContainer.append(headline);
+    contentBody.append(homeContainer);
+    content.append(contentBody);
 }
--- src/index.js
+++ src/index.js
@@ -4,41 +4,58 @@ import './styles.css';
 
 function main() {
     pageLoad();
-
+    
     const homeBtn = document.querySelector(".home");
     const menuBtn = document.querySelector(".menu");
     const contactBtn = document.querySelector(".contact");
 
     homeBtn.classList.add("selected");
+    home();
 
     homeBtn.addEventListener("click", () => {
+        const contentBody = document.querySelector(".content-body");
         homeBtn.classList.add("selected");
 
+        if (contentBody) {
+            contentBody.remove();
+        }
         if(menuBtn.classList.contains("selected")) {
             menuBtn.classList.toggle("selected");
         } else if(contactBtn.classList.contains("selected")) {
             contactBtn.classList.toggle("selected");
         }
+        home();
     });
 
     menuBtn.addEventListener("click", () => {
+        const contentBody = document.querySelector(".content-body");
         menuBtn.classList.add("selected");
+        
 
+        if (contentBody) {
+            contentBody.remove();
+        }
         if(homeBtn.classList.contains("selected")) {
             homeBtn.classList.toggle("selected");
         } else if(contactBtn.classList.contains("selected")) {
             contactBtn.classList.toggle("selected");
         }
+
     });
 
     contactBtn.addEventListener("click", () => {
+        const contentBody = document.querySelector(".content-body");
         contactBtn.classList.add("selected");
 
+        if (contentBody) {
+            contentBody.remove();
+        }
         if(homeBtn.classList.contains("selected")) {
             homeBtn.classList.toggle("selected");
         } else if(menuBtn.classList.contains("selected")) {
             menuBtn.classList.toggle("selected");
         }
+
     });
 }
 
